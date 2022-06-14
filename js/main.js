5841b87534d02c94883a4a01b94374cdf7c54c23
Ben King <benking1293@gmail.com>
2022-06-14 12:08:03 +0100

    Basic layout created and API connection to NASA established

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 //My NASA Key HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew
 
+let FHAZCam = "fhaz"
+
 fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=800&camera=fhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew')
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        document.querySelector('#FHAZImg').src = data.photos[0].img_src
+        document.querySelector('#RHAZImg').src = data.photos[0].img_src
 
                        
     })
@@ -11,10 +15,3 @@ fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=800&c
         console.log(`error ${err}`)
         alert("Error! Connection interrupted through Martian interference. Please repeat message, over.")
     })
-
-
-
-
-
-
-
