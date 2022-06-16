f2d084f58e3c3b819a2fe9cde4bc7750daa1819d
Ben King <benking1293@gmail.com>
2022-06-16 10:33:20 +0100

    Added postman fetch screenshot

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 //My NASA Key HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew
 
 document.querySelector('#pickSol').addEventListener('click', () => {
-    getFHAZ()
-    getRHAZ()
-    getNAVCAM()
+    let sol = document.querySelector('#solPick').value
+    getFHAZ(sol)
+    getRHAZ(sol)
+    getNAVCAM(sol)
 })
 
 
+function getFHAZ(solInput) {
 
-
-function getFHAZ() {
-
-let sol = document.querySelector('#solPick').value
-
-let nasaFHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=fhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
+let nasaFHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=fhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
 
 fetch(nasaFHAZURL)
     .then(res => res.json())
@@ -29,12 +26,9 @@ fetch(nasaFHAZURL)
 
 }
 
-function getRHAZ() {
+function getRHAZ(solInput) {
 
-let sol = document.querySelector('#solPick').value
-
-
-let nasaRHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=rhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
+let nasaRHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=rhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
 
 fetch(nasaRHAZURL)
     .then(res => res.json())
@@ -50,14 +44,11 @@ fetch(nasaRHAZURL)
 
 }
 
-function getNAVCAM() {
-
-let sol = document.querySelector('#solPick').value
+function getNAVCAM(solInput) {    
     
+let nasaNavcamURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=navcam&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
     
-let nasaRHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=navcam&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
-    
-    fetch(nasaRHAZURL)
+    fetch(nasaNavcamURL)
         .then(res => res.json())
         .then(data => {
             console.log(data)
@@ -68,4 +59,20 @@ let nasaRHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/phot
             console.log(`error ${err}`)
             alert("Error! Martian interference. Please repeat message, over.")
         })
+}
+
+function getManifest() {    
+    
+    let nasaManifestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/?api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
+        
+        fetch(nasaManifestURL)
+            .then(res => res.json())
+            .then(data => {
+                let maxSol = data.rover.max_sol
+                return maxSol
+            })
+            .catch(err => {
+                console.log(`error ${err}`)
+                alert("Error! Martian interference. Please repeat message, over.")
+            })
 }
