b9ee67dbb59ae79f7cce46a825ee47f56cc36a9a
Ben King <benking1293@gmail.com>
2022-07-04 15:50:19 +0100

    Landing sol and latest sol image fetch created. Can now click through each camera images taken on that day.

--- js/main.js
+++ js/main.js
@@ -10,30 +10,32 @@ document.querySelector('#pickSol').addEventListener('click', () => {
 // Click counter for sol's
 let count = 0
 
-// Minus a sol
+// Minus a sol images
 document.querySelector('#minusSol').addEventListener('click', () => {
-    this.onclick = function () {
     let sol = Number(document.querySelector('#solPick').value) + --count
     getFHAZ(sol)
     getRHAZ(sol)
     getNAVCAM(sol)
-    return count
-    }
-    
+    return count    
 })
 
-// Plus a sol
+// Plus a sol images
 document.querySelector('#plusSol').addEventListener('click', () => {
-    this.onclick = function () {
     let sol = Number(document.querySelector('#solPick').value) + ++count
     getFHAZ(sol)
     getRHAZ(sol)
     getNAVCAM(sol)
     return count
-    }
     
 })
 
+// Landing Sol images
+document.querySelector('#firstSol').addEventListener('click', () => {
+    let sol = 0
+    getFHAZ(sol)
+    getRHAZ(sol)
+    getNAVCAM(sol)
+})
 
 // Fetch FHAZ camera image
 function getFHAZ(solInput) {
@@ -45,6 +47,27 @@ fetch(nasaFHAZURL)
     .then(data => {
         console.log(data)
         document.querySelector('#FHAZImg').src = data.photos[0].img_src
+        fhazCount = 0
+        document.querySelector('#fhazArrowNext').addEventListener('click', () => {
+            if (fhazCount === data.photos.length -1) {
+                document.querySelector('#FHAZImg').src = data.photos[0].img_src
+                return fhazCount = 0
+            } else {
+                ++fhazCount
+                document.querySelector('#FHAZImg').src = data.photos[fhazCount].img_src
+                return fhazCount
+            }
+        })
+        document.querySelector('#fhazArrowPrevious').addEventListener('click', () => {
+            if (fhazCount <= 0) {
+                document.querySelector('#FHAZImg').src = data.photos[0].img_src
+                return fhazCount = 0
+            } else {
+                fhazCount--
+                document.querySelector('#FHAZImg').src = data.photos[fhazCount].img_src
+                return fhazCount
+            }
+        })
        
     })
     .catch(err => {
@@ -64,7 +87,27 @@ fetch(nasaRHAZURL)
     .then(data => {
         console.log(data)
         document.querySelector('#RHAZImg').src = data.photos[0].img_src
-        
+        rhazCount = 0
+        document.querySelector('#rhazArrowNext').addEventListener('click', () => {
+            if (rhazCount === data.photos.length -1) {
+                document.querySelector('#RHAZImg').src = data.photos[0].img_src
+                return rhazCount = 0
+            } else {
+                ++rhazCount
+                document.querySelector('#RHAZImg').src = data.photos[rhazCount].img_src
+                return rhazCount
+            }
+        })
+        document.querySelector('#rhazArrowPrevious').addEventListener('click', () => {
+            if (rhazCount <= 0) {
+                document.querySelector('#RHAZImg').src = data.photos[0].img_src
+                return rhazCount = 0
+            } else {
+                rhazCount--
+                document.querySelector('#RHAZImg').src = data.photos[rhazCount].img_src
+                return rhazCount
+            }
+        })
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -83,7 +126,27 @@ let nasaNavcamURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/ph
         .then(data => {
             console.log(data)
             document.querySelector('#NAVCAMImg').src = data.photos[0].img_src
-            
+            navCount = 0
+        document.querySelector('#navArrowNext').addEventListener('click', () => {
+            if (navCount === data.photos.length -1) {
+                document.querySelector('#NAVCAMImg').src = data.photos[0].img_src
+                return navCount = 0
+            } else {
+                ++navCount
+                document.querySelector('#NAVCAMImg').src = data.photos[navCount].img_src
+                return navCount
+            }
+        })
+        document.querySelector('#navArrowPrevious').addEventListener('click', () => {
+            if (navCount <= 0) {
+                document.querySelector('#NAVCAMImg').src = data.photos[0].img_src
+                return navCount = 0
+            } else {
+                navCount--
+                document.querySelector('#NAVCAMImg').src = data.photos[navCount].img_src
+                return navCount
+            }
+        })
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -91,20 +154,25 @@ let nasaNavcamURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/ph
         })
 }
 
-// //
-// function getManifest() {    
-    
-//     let nasaManifestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/?api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
-        
-//         fetch(nasaManifestURL)
-//             .then(res => res.json())
-//             .then(data => {
-//                 let maxSol = data.rover.max_sol
-//                 return maxSol
-//             })
-//             .catch(err => {
-//                 console.log(`error ${err}`)
-//                 alert("Error! Martian interference. Please repeat message, over.")
-//             })
-// }
 
+    
+let nasaManifestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/?api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
+    
+    fetch(nasaManifestURL)
+        .then(res => res.json())
+        .then(data => {
+            let maxSol = data.rover.max_sol
+         
+            // Fetch latest sol images on click
+            document.querySelector('#latestSol').addEventListener('click', () => {
+                getFHAZ(maxSol)
+                getRHAZ(maxSol)
+                getNAVCAM(maxSol)
+            })
+        
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+            alert("Error! Martian interference. Please repeat message, over.")
+        })
