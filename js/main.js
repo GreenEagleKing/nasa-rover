117ccd67e012bf917de3eea0b9592c76ab8c92d0
Ben King <benking1293@gmail.com>
2022-07-04 11:42:49 +0100

    Added the addition and minus functionality to yesterday and next sol

--- js/main.js
+++ js/main.js
@@ -7,7 +7,35 @@ document.querySelector('#pickSol').addEventListener('click', () => {
     getNAVCAM(sol)
 })
 
+// Click counter for sol's
+let count = 0
 
+// Minus a sol
+document.querySelector('#minusSol').addEventListener('click', () => {
+    this.onclick = function () {
+    let sol = Number(document.querySelector('#solPick').value) + --count
+    getFHAZ(sol)
+    getRHAZ(sol)
+    getNAVCAM(sol)
+    return count
+    }
+    
+})
+
+// Plus a sol
+document.querySelector('#plusSol').addEventListener('click', () => {
+    this.onclick = function () {
+    let sol = Number(document.querySelector('#solPick').value) + ++count
+    getFHAZ(sol)
+    getRHAZ(sol)
+    getNAVCAM(sol)
+    return count
+    }
+    
+})
+
+
+// Fetch FHAZ camera image
 function getFHAZ(solInput) {
 
 let nasaFHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=fhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
@@ -26,6 +54,7 @@ fetch(nasaFHAZURL)
 
 }
 
+// Fetch RHAZ camera image
 function getRHAZ(solInput) {
 
 let nasaRHAZURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=rhaz&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
@@ -44,6 +73,7 @@ fetch(nasaRHAZURL)
 
 }
 
+// Fetch NAVCAM camera image
 function getNAVCAM(solInput) {    
     
 let nasaNavcamURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solInput}&camera=navcam&api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
@@ -61,18 +91,20 @@ let nasaNavcamURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/ph
         })
 }
 
-function getManifest() {    
+// //
+// function getManifest() {    
     
-    let nasaManifestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/?api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
+//     let nasaManifestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/?api_key=HTn441SSfabqhV7KiFBa5KJYXERa6P5q2vZcb5ew`
         
-        fetch(nasaManifestURL)
-            .then(res => res.json())
-            .then(data => {
-                let maxSol = data.rover.max_sol
-                return maxSol
-            })
-            .catch(err => {
-                console.log(`error ${err}`)
-                alert("Error! Martian interference. Please repeat message, over.")
-            })
-}
+//         fetch(nasaManifestURL)
+//             .then(res => res.json())
+//             .then(data => {
+//                 let maxSol = data.rover.max_sol
+//                 return maxSol
+//             })
+//             .catch(err => {
+//                 console.log(`error ${err}`)
+//                 alert("Error! Martian interference. Please repeat message, over.")
+//             })
+// }
+
